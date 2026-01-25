622561b85e38e097292169b2d141913863a72e39
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-01-25 11:47:37 +0600

    fixed the app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <script setup>
-  import ReadyToShip from "./components/custom/ReadyToShip.vue";
-  import ExploreFeatures from "./components/custom/ExploreFeatures.vue";
-  import FeatureBenefits from "./components/custom/FeatureBenefits.vue";
-  import Hero from "./components/custom/Hero.vue";
   import Navbar from "./components/custom/Navbar.vue";
   import Footer from "./components/custom/Footer.vue";
-  import WhatMakesDifferent from "./components/custom/WhatMakesDifferent.vue";
-  import ChooseAPlan from "./components/custom/ChooseAPlan.vue";
   import { RouterView } from "vue-router";
 </script>
 
