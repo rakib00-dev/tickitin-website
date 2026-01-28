138db7abcea4db30829080c03fe030197b9c3c5f
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-01-28 16:11:14 +0600

    updated the main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
+import { createHead } from "@unhead/vue/server";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+const head = createHead();
+
+app.use(head).use(router).mount("#app");
