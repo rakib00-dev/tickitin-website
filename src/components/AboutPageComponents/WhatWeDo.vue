5d4bbeecd1291e2e16dab6f0591abbdee6991c5c
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-02-06 15:54:30 +0600

    updated the captain and what we do

--- src/components/AboutPageComponents/WhatWeDo.vue
+++ src/components/AboutPageComponents/WhatWeDo.vue
@@ -15,30 +15,7 @@
           class="text-sm md:text-lg font-medium text-gray-700 text-left md:px-4"
         >
           Tickitin is a social-first, no-fee ticket marketplace. We combine:
-          <span
-            class="rounded-full border-2 bg-gray-400 text-white px-2.5 py-1 text-[.7rem] font-bold w-fit"
-            >1</span
-          >
-          event discovery
-          <span
-            class="rounded-full border-2 bg-gray-400 text-white px-2.5 py-1 text-[.7rem] font-bold w-fit"
-            >2</span
-          >
-          group planning
-          <span
-            class="rounded-full border-2 bg-gray-400 text-white px-2.5 py-1 text-[.7rem] font-bold w-fit"
-            >3</span
-          >
-          split payments
-          <span
-            class="rounded-full border-2 bg-gray-400 text-white px-2.5 py-1 text-[.7rem] font-bold w-fit"
-            >4</span
-          >
-          AI assistance
-          <span
-            class="rounded-full border-2 bg-gray-400 text-white px-2.5 py-1 text-[.7rem] font-bold w-fit"
-            >5</span
-          >
+          event discovery, group planning, split payments, AI assistance,
           verified ticket inventory …into one simple experience. So instead of
           juggling five apps, you open one. Find something fun. Invite friends.
           Pay instantly. Show up. That&apos;s it
