b44ed097d1f1ba60e8697a2de03b95fe7cd66ff4
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-02-20 17:04:47 +0600

    removed the about ticket sports

--- src/components/SportsPageComponents/AboutTicketSports.vue
+++ src/components/SportsPageComponents/AboutTicketSports.vue
@@ -8,31 +8,6 @@
       <header class="mb-8">
         <h1 class="text-4xl font-bold mb-2">About Tickitin Sports</h1>
       </header>
-
-      <!-- <section class="mb-6 leading-relaxed">
-        <p>
-          These Terms of Service (“Terms”) are a binding agreement between you
-          and <strong>Tickitin Experiences LLC</strong> (“Tickitin,” “we,”
-          “us”). They govern your access to and use of Tickitin{{ `’` }}s
-          websites, mobile applications, and related services (collectively, the
-          “Platform”).
-        </p>
-      </section>
-
-      <section class="space-y-4 leading-relaxed">
-        <div class="border border-gray-300 rounded-lg p-4">
-          <h2 class="text-xl font-semibold mb-2">Contact</h2>
-          <p>
-            Questions about these Terms may be sent to
-            <a href="mailto:support@tickitin.com" class="text-blue-600 ml-1">
-              support@tickitin.com.
-            </a>
-            <RouterLink to="/contact-us" class="font-bold !underline">
-              Visit Contact</RouterLink
-            >
-          </p>
-        </div>
-      </section> -->
     </article>
   </main>
 </template>
